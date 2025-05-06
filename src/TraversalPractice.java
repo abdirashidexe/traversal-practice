ee6b6d3b66a8477ab7390291f29ea43a088639ea
Abdi Ahmed <89213120+abdirashidexe@users.noreply.github.com>
2025-05-05 21:42:52 -0700

    printNodesWithOneChild solved

--- src/TraversalPractice.java
+++ src/TraversalPractice.java
@@ -31,7 +31,16 @@ public class TraversalPractice {
    * @param node The root of the tree to print
    */
   public static <T> void printNodesWithOneChild(Node<T> node) {
-    
+    if (node != null)
+    {
+      if ((node.left != null && node.right == null) || (node.left == null && node.right != null)) // if either of them have content
+      {
+        System.out.println(node.value);
+      }
+      
+      printNodesWithOneChild(node.left);
+      printNodesWithOneChild(node.right);
+    }
   }
 
     /**
